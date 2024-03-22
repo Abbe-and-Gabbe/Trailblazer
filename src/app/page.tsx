2b2ec0b192927e4d261266b1bcffbce9efdaafeb
Albin Forsberg <albin02forsberg@gmail.com>
2024-03-22 22:04:59 +0100

    Working on nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import Image from "next/image";
 
+// Set the page title
+import Head from "next/head";
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    document.title = "Trailblazer";
+  }, []);
+
   return (
-    <div>Test</div>
+    <div>
+      <Head>
+        <title>Trailblazer</title>
+      </Head>
+      <header>
+        <h1>Welcome to Trailblazer</h1>
+        <p>Data visualization with the WHAT-WHY-HOW method</p>
+      </header>
+    </div>
   );
 }
